f5f76d9d64e87b2562f5400bf9cb5e78b5951c6e
Bredik <br-bred@yandex.ru>
2021-07-09 10:50:44 +0300

    delete v1

--- bank-api-v1/src/main/java/ru/sbr/utils/ParserURI.java
+++
@@ -1,9 +0,0 @@
-package ru.sbr.utils;
-
-public class ParserURI {
-    public long getNumberFromUri(String uri) {
-        System.out.println("Получаем айди из урла для добавления карты");
-        String[] strings = uri.split("/");
-        return Long.parseLong(strings[strings.length - 1]);
-    }
-}
